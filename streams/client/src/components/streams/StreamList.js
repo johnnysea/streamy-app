1a125d4e3f0ca63b977c764262596754900e55ef
Johnny Shao <39663313+johnnysea@users.noreply.github.com>
2020-02-22 16:15:27 +1300

    reuse redux form and implement streamEdit

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 class StreamList extends React.Component {
@@ -13,9 +13,10 @@ class StreamList extends React.Component {
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
                         Edit
-                    </button>
+                    </Link>
+
                     <button className="ui button negative">
                         Delete
                     </button>
@@ -24,10 +25,10 @@ class StreamList extends React.Component {
         }
     }
 
-    renderCreate(){
-        if (this.props.isSignedIn){
-            return(
-                <div style={{textAlign: 'right'}}>
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
                     <Link to="/streams/new" className="ui button primary" >
                         Create Stream
                     </Link>
